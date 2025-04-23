64849954bcb2841f11c8d4154709c72af6900741
itayash22 <itayash@gmail.com>
2025-04-23 09:10:39 +0300

    Create initCropper.js
    created file

---
+++ js/initCropper.js
@@ -0,0 +1,16 @@
+// Imports the ESM build of Cropper.js from CDN
+i mport Cropper from 'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.esm.js';
+
+export function setupCropper(imgEl) {
+  return new Cropper(imgEl, {
+    viewMode: 1,
+    dragMode: 'crop',
+    autoCropArea: 0.7,
+    movable: true,
+    zoomable: true,
+    zoomOnTouch: true,
+    cropBoxMovable: true,
+    cropBoxResizable: true,
+    responsive: true,
+  });
+}
